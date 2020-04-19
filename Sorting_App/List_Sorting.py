fee1d53b47c6c8e39ef8f9014035bdbb79aca99e
Sojan S R <sojansgithub@outlook.com>
2020-04-19 13:25:29 +0530

    adding range sum functions

--- Sorting_App/List_Sorting.py
+++ Sorting_App/List_Sorting.py
@@ -2,4 +2,20 @@ def linear_sort(list):
     #somecode here
     print("Sorted list comes out here:")
 
-linear_sort(["",""])
+def multiply_or_switch(a,b):
+    if a*b < 1000:
+        return a*b
+    else:
+        return a+b
+
+def range_sum():
+    _get =input("Enter the range number:")
+    _input = int(_get)
+    sum = 0
+    for x in range(0,_input,):
+        print("Previous sum = " + str(sum))
+        sum = sum + x
+        print("Present sum = " + str(sum))
+
+range_sum()
+
