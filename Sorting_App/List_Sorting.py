06f5a8bc57a97f7c2e266c07cbf3d873ad0526aa
sojansr <sojansr@outlook.com>
2020-04-23 18:26:44 +0530

    adding excersice files

--- Sorting_App/List_Sorting.py
+++ Sorting_App/List_Sorting.py
@@ -1,3 +1,30 @@
+# -- Function to search sub string in a given string 
+def substring_search():
+    _input_string = input("Enter the string to search in: ")
+    _search_string = input("Enter the string to search: ")
+    _split_list = _input_string.split(" ")
+    _count = 0
+    for x in range(0, len(_split_list)):
+        if _split_list[x] == _search_string:
+            _count = _count +1
+    print("{0} appeared {1} times".format(_search_string,_count))
+
+# -- Palidrome Check function
+def palindrome_check():
+    number = int(input('Enter the number: '))
+    print("original number", number)
+    originalNum = number
+    reverseNum = 0
+    while (number > 0):
+        reminder = number % 10
+        reverseNum = (reverseNum * 10) + reminder
+        number = number // 10
+    if (originalNum == reverseNum):
+        print("True")
+    else:
+        print("False")
+palindrome_check()
+
 def linear_sort(list):
     #somecode here
     print("Sorted list comes out here:")
@@ -17,5 +44,34 @@ def range_sum():
         sum = sum + x
         print("Present sum = " + str(sum))
 
-range_sum()
+def list_search():
+    #enter the list size of the list
+    _list_size = int(input("Enter the max index of list: "))
+    
+    #declare the list variable
+    _list = []
 
+    #list iteration to fetch the list values
+    try:
+        for x in range(0,_list_size):
+            _list.append(int(input("Enter the {0} th element: ".format(x))))
+    except:
+        print("Sorry ! Ivalid entry or Not enough entries !")
+    if _list[0] == _list[x]:
+        print("First and last elements are the same !")
+    else:
+        print("First and last elements are different !")
+
+# -- Function to return list members that are divisible by 5
+def check_div_by_five():
+    _list_size = int(input("Enter the list size: "))
+    _list = []
+
+    try:
+        for x in range(0,_list_size):
+            _list.append(int(input("Enter the {0}the element: ".format(x))))
+    except:
+        print("Sorry ! Invalid Entry or Doesn't match list size !")
+    for x in range(0,_list_size):
+        if _list[x]%5 == 0:
+            print(_list[x])
